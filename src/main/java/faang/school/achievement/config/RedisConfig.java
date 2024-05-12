fbef503db7031c427e9e19a625f81f53f1209df1
Sam Migachev <sammigachev@gmail.com>
2024-05-12 13:48:56 +0300

    pull request

--- src/main/java/faang/school/achievement/config/RedisConfig.java
+++ src/main/java/faang/school/achievement/config/RedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -59,5 +58,4 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
-
 }
--- src/main/java/faang/school/achievement/listener/SkillEventListener.java
+++ src/main/java/faang/school/achievement/listener/SkillEventListener.java
@@ -3,10 +3,6 @@ package faang.school.achievement.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.achievement.event.SkillAcquiredEvent;
 import faang.school.achievement.handler.SkillFullAchievementHandler;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
 @Component
